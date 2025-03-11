855d97e1e6a4e7be03775be91cb7c9b5b430d22a
Micah Huber <pfp2024@gmail.com>
2025-03-11 11:47:46 -0500

    Fix file references

--- modules/programs/scripts.nix
+++ modules/programs/scripts.nix
@@ -13,11 +13,18 @@
       fileLinks = lib.mapAttrs' 
         (name: _: {
           name = "scripts/${name}";
-          value = { source = "${filesDir}/${name}"; };
+          value = { source = "${filesDir}/scripts/${name}"; };
         })
         files;
     in
-      fileLinks;
+      fileLinks // {
+        # Add the .keep file to the same set
+        "scripts/.keep" = { text = ""; };
+      };
 
-  #home.file."scripts/.keep".text = "";
+  home.activation = {
+    makeScriptsExecutable = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      find $HOME/scripts -type f -not -path "*/\.*" -exec chmod +x {} \;
+    '';
+  };
 }
