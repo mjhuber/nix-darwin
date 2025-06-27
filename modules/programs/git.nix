676131a4a059f5d411766586172bf1b00bcd2679
Micah Huber <pfp2024@gmail.com>
2025-06-26 21:51:43 -0400

    remove unnecessary comments

--- modules/programs/git.nix
+++ modules/programs/git.nix
@@ -18,10 +18,5 @@
   home.file."${config.home.homeDirectory}/.git-template" = {
     source = "${filesDir}/git-templates";
     recursive = true;
-    # IMPORTANT: If any files within git-templates need to be executable (like pre-commit),
-    # you might need to set their permissions in your source directory
-    # or handle them individually if permissions vary.
-    # Home Manager will preserve source permissions when copying recursively.
-    # For common git hooks, it's good practice to make them executable in your source.
   };
 }
