465afe884bb2b71444c2fa5ac1058d99743ca45c
Micah Huber <pfp2024@gmail.com>
2025-10-04 19:00:49 -0400

    remove unused binding

--- modules/programs/scripts.nix
+++ modules/programs/scripts.nix
@@ -1,29 +1,32 @@
 {
   lib,
-  pkgs,
   filesDir,
   ...
-}: {
-  home.file = 
+}:
+{
+  home.file =
     let
       # Get all files in the directory
       files = builtins.readDir "${filesDir}/scripts";
-      
+
       # Create a set mapping each file to its source
-      fileLinks = lib.mapAttrs' 
-        (name: _: {
-          name = "scripts/${name}";
-          value = { source = "${filesDir}/scripts/${name}"; };
-        })
-        files;
+      fileLinks = lib.mapAttrs' (name: _: {
+        name = "scripts/${name}";
+        value = {
+          source = "${filesDir}/scripts/${name}";
+        };
+      }) files;
     in
-      fileLinks // {
-        # Add the .keep file to the same set
-        "scripts/.keep" = { text = ""; };
+    fileLinks
+    // {
+      # Add the .keep file to the same set
+      "scripts/.keep" = {
+        text = "";
       };
+    };
 
   home.activation = {
-    makeScriptsExecutable = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    makeScriptsExecutable = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
       find $HOME/scripts -type f -not -path "*/\.*" -exec chmod +x {} \;
     '';
   };
