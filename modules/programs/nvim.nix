20139bf83d85824834bdcbb3fffb2bcdab8d3e80
Micah Huber <pfp2024@gmail.com>
2025-10-11 10:00:27 -0400

    move nvim configs to their own dir

--- modules/programs/nvim.nix
+++ modules/programs/nvim.nix
@@ -1,5 +1,6 @@
 {
   pkgs,
+  lib,
   filesDir,
   ...
 }: {
@@ -7,13 +8,34 @@
     neovim
   ];
 
-  home.file.".config/nvim/init.vim".source = "${filesDir}/init.vim";
+  # home.file.".config/nvim/init.vim".source = "${filesDir}/init.vim";
 
-  # vim-plug
-  home.file.".local/share/nvim/site/autoload/plug.vim" = {
-    source = pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim";
-      sha256 = "1nywzjd9nfr7sqqbdi69wza305q3vp26i0390j1884wdz6awid10";
+  home.file =
+    let
+      # Get all files in the directory
+      files = builtins.readDir "${filesDir}/nvim";
+
+      # Create a set mapping each file to its source
+      fileLinks = lib.mapAttrs' (name: _: {
+        name = ".config/nvim/${name}";  # Remove the ~
+        value = {
+          source = "${filesDir}/nvim/${name}";
+        };
+      }) files;
+    in
+    fileLinks
+    // {
+      # Add the .keep file to the same set
+      "nvim/.keep" = {
+        text = "";
+      };
+
+      # vim-plug
+      ".local/share/nvim/site/autoload/plug.vim" = {
+        source = pkgs.fetchurl {
+          url = "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim";
+          sha256 = "1nywzjd9nfr7sqqbdi69wza305q3vp26i0390j1884wdz6awid10";
+        };
+      };
     };
-  };
 }
